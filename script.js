7973472fc01f466119eff0840bc5393627b22615
daferrom <daferrom@gmail.com>
2021-11-14 16:50:33 -0500

    fixing burguer menu

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //Menu 
 const btn_menu = document.querySelector('.btn_menu')
+const menu_items = document.querySelector('.menuItems')
 
-addEventListener('DOMContentLoaded',()=>{
+addEventListener('DOMContentLoaded', () => {
     if (btn_menu){
         btn_menu.addEventListener('click', ()=>{
-            const menu_items = document.querySelector('.menuItems')
             menu_items.classList.toggle('show')
         })
     }
