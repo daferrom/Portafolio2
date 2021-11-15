2af9070a84c24f484b084d8aadc286443c16acef
daferrom <daferrom@gmail.com>
2021-11-15 17:11:04 -0500

    burger menu fixed

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 //Menu 
-const btn_menu = document.querySelector('.btn_menu')
-const menu_items = document.querySelector('.menuItems')
+
+
 
 addEventListener('DOMContentLoaded', () => {
-    if (btn_menu){
-        btn_menu.addEventListener('click', ()=>{
+    const btn_menu = document.querySelector('.btn_menu')
+    const listItems = document.querySelector('.listItem')
+    console.log(listItems)
+     if (btn_menu){
+        btn_menu.addEventListener('click', () =>{
+            const menu_items = document.querySelector('.menuItems')
             menu_items.classList.toggle('show')
         })
-    }
+    } 
 })
+
+
+
 
 
 
