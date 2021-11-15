27bf556986c4582da5fad7c16ed67bd3b669e80a
daferrom <daferrom@gmail.com>
2021-11-15 17:20:43 -0500

    fixing burger menu

--- scriptmenu.js
+++ scriptmenu.js
@@ -5,7 +5,7 @@
 addEventListener('DOMContentLoaded', () => {
     const btn_menu = document.querySelector('.btn_menu')
     const listItems = document.querySelector('.listItem')
-    console.log(listItems)
+    
      if (btn_menu){
         btn_menu.addEventListener('click', () =>{
             const menu_items = document.querySelector('.menuItems')
